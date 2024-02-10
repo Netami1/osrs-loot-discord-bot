67fdb9916f6781ba6a5e5eac770b0b42ae577613
Netami <thenetami1@gmail.com>
2024-02-10 18:56:49 -0500

    Why isn't this class found

--- app/Listeners/TestCommandListener.php
+++ app/Listeners/TestCommandListener.php
@@ -3,10 +3,9 @@
 namespace App\Listeners;
 
 use Illuminate\Support\Facades\Log;
-use Nwilging\LaravelDiscordBot\Contracts\Listeners\ApplicationCommandInteractionEventListenerContract;
 use Nwilging\LaravelDiscordBot\Events\ApplicationCommandInteractionEvent;
 
-class TestCommandListener implements ApplicationCommandInteractionEventListenerContract
+class TestCommandListener
 {
     /**
      * Create the event listener.
@@ -19,23 +18,8 @@ class TestCommandListener implements ApplicationCommandInteractionEventListenerC
     /**
      * Handle the event.
      */
-    public function replyContent(ApplicationCommandInteractionEvent $event): ?string
-    {
-        return 'loading';
-    }
-
-    public function behavior(ApplicationCommandInteractionEvent $event): int
-    {
-        return static::LOAD_WHILE_HANDLING;
-    }
-
-    public function command(): ?string
-    {
-        return 'test-command';
-    }
-
     public function handle(ApplicationCommandInteractionEvent $event): void
     {
-        Log::info("Handled " . $event->getCommandName());
+        Log::info('Test123');
     }
 }
