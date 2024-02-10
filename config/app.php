2b45f067296382cdb18160ad65678e9e44230fb1
Netami <thenetami1@gmail.com>
2024-02-10 18:23:27 -0500

    Adds discord service provider

--- config/app.php
+++ config/app.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
+use Nwilging\LaravelDiscordBot\Providers\DiscordBotServiceProvider;
 
 return [
 
@@ -168,6 +169,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        DiscordBotServiceProvider::class,
     ])->toArray(),
 
     /*
