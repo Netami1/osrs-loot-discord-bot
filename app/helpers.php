190e18c2e168194da8f75e333b55693f71b1e62d
Netami <thenetami1@gmail.com>
2024-02-11 14:04:36 -0500

    Oops

--- app/helpers.php
+++ app/helpers.php
@@ -3,14 +3,17 @@
 if (!function_exists('kmb')) {
     function kmb($number)
     {
-        $suffix = '';
+        if ($number < 1000) {
+            return $number;
+        }
+
         if ($number >= 1000000000) {
             $number = $number / 1000000000;
             $suffix = 'b';
         } elseif ($number >= 1000000) {
             $number = $number / 1000000;
             $suffix = 'm';
-        } elseif ($number >= 1000) {
+        } else {
             $number = $number / 1000;
             $suffix = 'k';
         }
